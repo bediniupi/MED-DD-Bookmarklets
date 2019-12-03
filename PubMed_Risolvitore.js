c2ecea9db64bf40d6a73222f5a921e33b318a852
Nazzareno Bedini <33749413+bediniupi@users.noreply.github.com>
2019-12-03 08:26:23 +0100

    New PubMed UI

--- PubMed_Risolvitore.js
+++ PubMed_Risolvitore.js
@@ -1,2 +1,6 @@
-var pmid = $('.rprtid dd:first').text();
+var pmid = $('strong:first[title="PubMed ID"]').text();
+if (!pmid) {
+ alert("PMID non trovato.");   
+ return;
+}
 window.open("http://medina.sba.unipi.it/intranet/acnp/pubmed_acnp.php" + "?pmid=" + pmid);
